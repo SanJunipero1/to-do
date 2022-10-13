54ce911db20ba7fd0ecde511803bcaa85ecc6a74
SanJunipero1 <110401109+SanJunipero1@users.noreply.github.com>
2022-10-13 15:00:46 +0200

    update2

--- javascript/script.js
+++ javascript/script.js
@@ -1,89 +1,119 @@
-const checkItem = document.getElementById("check");
-const deleteItem = document.getElementById("delete");
-const editItem = document.getElementById("edit");
+const checkItemx = document.getElementById("check");
+const deleteItemX = document.getElementById("delete");
+const editItemx = document.getElementById("edit");
+const topDiv = document.getElementById("topDiv")
+const bottomDiv = document.getElementById("bottomDiv")
 
-/**
- * Toggle Modal
- */
-const addButton = document.querySelector("#open-dialog");
-const modal = document.querySelector(".addTodo dialog");
 
-function toggleModal() {
-  modal.show();
+ const addButton = document.querySelector("#open-dialog");
+ const modal = document.querySelector(".addTodo dialog");
+ 
+ function toggleModal() {
+   modal.show();
+ }
+ addButton.addEventListener("click", toggleModal);
+ 
+
+ function setAttributes(element, attributes) {
+   Object.keys(attributes).forEach((attr) => {
+     element.setAttribute(attr, attributes[attr]);
+   });
+ }
+
+ 
+ const inputField = document.getElementById("todoTitle");
+const listContainer = document.getElementById("listContainer")
+
+let inputList = [];
+
+function handleInput(event){
+  const newInput = event.target.value
+  inputList.push({newInput})
+  inputField.value = "";
+  createList(inputList);
+  console.log(inputList)
 }
-addButton.addEventListener("click", toggleModal);
+inputField.addEventListener("change", handleInput);
 
-/**
- * Helper Function for adding Attributes
- */
-function setAttributes(element, attributes) {
-  Object.keys(attributes).forEach((attr) => {
-    element.setAttribute(attr, attributes[attr]);
-  });
+  
+
+function createList(liste){
+  topDiv.innerHTML ="";
+  liste.forEach((item, index)=>{
+          const div = document.createElement("div")
+          div.style.width = "690px";
+          div.style.height = "40px";
+          div.style.display = "flex";
+          div.style.margin = "10px";
+          div.style.justifyContent = "center";
+          const ul = document.createElement("ul")
+          const li = document.createElement("li");
+          li.innerText = item.newInput;
+          li.setAttribute("id", `element${index}`)
+          const checkButton = document.createElement("button");
+          checkButton.style.width = "40px";
+          checkButton.style.height = "40px";
+          checkButton.style.backgroundImage = "url(istockphoto-1129153620-170667a.jpg)";
+          checkButton.style.backgroundSize = "40px";
+          checkButton.style.border = "0px";
+          checkButton.style.cursor = "pointer";
+          checkButton.style.margin = "5px 10px";
+          checkButton.innerText = "";
+          const deleteButton = document.createElement("button");
+          deleteButton.style.width = "30px";
+          deleteButton.style.height = "30px";
+          deleteButton.style.backgroundImage = "url(download.png)";
+          deleteButton.style.backgroundSize = "30px";
+          deleteButton.style.border = "0px";
+          deleteButton.style.cursor = "pointer";
+          deleteButton.style.margin = "5px 10px";
+          deleteButton.innerText = "";
+          const editButton = document.createElement("button");
+          editButton.style.width = "30px";
+          editButton.style.height = "30px";
+          editButton.style.backgroundImage = "url(download-1.png)";
+          editButton.style.backgroundSize = "30px";
+          editButton.style.border = "0px";
+          editButton.style.cursor = "pointer";
+          editButton.style.margin = "5px 10px";
+          editButton.innerText = "";
+          
+          topDiv.appendChild(div);
+          div.appendChild(checkButton);
+          div.appendChild(ul)
+          ul.appendChild(li);
+          div.appendChild(editButton);
+          div.appendChild(deleteButton);
+         
+          
+          //handle check
+          checkButton.addEventListener("click",()=>{
+            bottomDiv .appendChild(ul);
+            div.innerHTML =""
+          })
+          
+          //handle edit
+          editButton.addEventListener("click",()=>{
+            let neuerText = prompt("");
+            li.innerText = neuerText
+          })
+
+          //handle delete
+          deleteButton.addEventListener("click", ()=>{
+            deleteItem(index);
+          } )
+
+          
+      })
+      
+
 }
 
-/**
- * Add ToDo Add Functionality
- */
-const toDoTopContainer = document.querySelector(".toDoTop");
-const submit = document.querySelector("#submit");
-const inputHeadline = document.querySelector("input#todoTitle");
-let toDoHeadline = "";
-let toDoArray = [];
-let i;
 
-function createToDo(headlineContent = "Platzhalter") {
-  //iteration for each element in Array so every Element can have a unique Id
-  for (i = 0; i <= toDoArray.length; i++) {}
-  const toDoOuter = document.createElement("div");
-  const toDoOuterAttributes = { class: "listItem", id: `toDo${i}` };
-  setAttributes(toDoOuter, toDoOuterAttributes);
-  toDoOuter.setAttribute("class", `listItem`);
 
-  //Create done Checkbox
-  const doneOuter = document.createElement("div");
 
-  const doneInput = document.createElement("button");
-  const inputAttributes = { id: "class" };
-  setAttributes(doneInput, inputAttributes);
-  doneOuter.append(doneInput);
+function deleteItem(index){
+  inputList.splice(index, 1);
+  createList(inputList)
 
-  //Create ToDo Headline
-  const headlineOuter = document.createElement("div");
-  const headline = document.createElement("h2");
-  headline.innerText = headlineContent;
-  headlineOuter.appendChild(headline);
-
-  //Create Edit Button
-  const editBtnOuter = document.createElement("div");
-  const editBtn = document.createElement("button");
-  editBtn.setAttribute("class", "edit");
-  editBtnOuter.appendChild(editBtn);
-
-  //Create Delete Button
-  const delBtnOuter = document.createElement("div");
-  const delBtn = document.createElement("button");
-  delBtn.setAttribute("class", "delete");
-  delBtnOuter.appendChild(delBtn);
-
-  //Append completed ToDo
-  toDoOuter.append(doneOuter, headlineOuter, editBtnOuter, delBtnOuter);
-  toDoTopContainer.appendChild(toDoOuter);
-
-  //Add ToDo to Array
-  toDoArray.push(toDoOuter);
-  console.log(toDoArray);
 }
-
-/**
- * EventListener für Add Funktion
- */
-function handleChange(event) {
-  toDoHeadline = event.target.value;
-}
-inputHeadline.addEventListener("change", handleChange);
-
-submit.addEventListener("click", () => {
-  inputHeadline.value = "";
-  createToDo(toDoHeadline);
-});
